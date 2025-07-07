dc36f1fc6d60e142fb3ac9e3e23e0109352fdade
GalYazdi <galyazdi11@gmail.com>
2025-07-07 11:36:22 +0300

    Replaces fake movie data generation with static mock data
    Switches from dynamically generating movie data using Faker.js to using predefined mock data from a JSON file

--- src/api/fakeMovies.ts
+++ src/api/fakeMovies.ts
@@ -1,16 +1,7 @@
-// import type { Movie } from "../types/Movie";
-import { faker } from "@faker-js/faker";
 import type { Movie } from "debflix-types";
+import data from "./mocks/movies.json";
 
 export const getMostLikedMovie = async (): Promise<Movie[]> => {
   await new Promise((res) => setTimeout(res, 300));
-  return Array.from({ length: 10 }, () => ({
-    id: faker.string.uuid(),
-    title: faker.lorem.words(3),
-    rating: parseFloat(faker.number.float({ min: 5, max: 10 }).toFixed(1)),
-    categories: [faker.music.genre(), faker.music.genre(), faker.music.genre()],
-    year: faker.date.past({ years: 25 }).getFullYear(),
-    director: faker.person.fullName(),
-    likes: faker.number.int({ min: 1, max: 1000 }),
-  }));
+  return data;
 };
--- src/pages/homePage/MoviesList.tsx
+++ src/pages/homePage/MoviesList.tsx
@@ -1,11 +1,16 @@
 import type { Movie } from "debflix-types";
 import React from "react";
 import { MovieCard } from "../../components/MovieCard";
+import styles from "./MoviesList.module.css"
 
 type Props = {
   movies: Movie[];
 };
 
 export const MoviesList = ({ movies }: Props) => {
-  return movies.map((movie) => <MovieCard key={movie.id} movie={movie} />);
+  return movies.map((movie) => (
+    <div >
+      <MovieCard key={movie.id} movie={movie} />)
+    </div>
+  ));
 };
